377c7da52330a830902b7589a348f45ab2d59e02
megavolkan <megavolkan@gmail.com>
2022-06-19 18:19:14 +0300

    Browser sync'i çalıştırmayı deniyorum

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,29 @@ import fileinclude from 'gulp-file-include'
 import dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass)
+import browserSync from 'browser-sync';
+const server = browserSync.create();
 
+const paths = {
+  styles: {
+    src: 'src/sass/**/*.sass',
+    dest: 'dist/assets/css'
+  },
+  scripts: {
+    src: 'src/scripts/*.js',
+    dest: 'dist/scripts/'
+  },
+  views: {
+    src: 'src/views/**/*.html',
+    dest: 'dist/'
+  },
+};
 
 // Copy all html files to dist dir, fonksiyona istedğin ismi verebilirsin-> [copyHtml]
 gulp.task('copyHtml', async () => {
   gulp.src('src/*.html') // Kaynak html dosyaları
     .pipe(gulp.dest('dist')) // Output folder ismi
 })
-
 
 // Optimize images
 gulp.task('imagemin', async () => {
@@ -22,7 +37,7 @@ gulp.task('imagemin', async () => {
 
 // File includes
 gulp.task('fileinclude', async () => {
-  gulp.src(['src/views/**/*.html','!src/views/**/_*.html'])
+  gulp.src([paths.views.src,'!src/views/**/_*.html'])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
@@ -32,17 +47,37 @@ gulp.task('fileinclude', async () => {
 
 // Compile Sass
 gulp.task('sass', async () => {
-    gulp.src('src/sass/**/*.scss', { sourcemaps: true })
-      .pipe(sass.sync().on('error', sass.logError)) // scss to css
-      .pipe(gulp.dest('dist/assets/css', { sourcemaps: '../sourcemaps/' }))
+  gulp.src(paths.styles.src, { sourcemaps: true })
+    .pipe(sass.sync().on('error', sass.logError)) // scss to css
+    .pipe(gulp.dest(paths.styles.dest, { sourcemaps: '../sourcemaps/' }))
 })
 
+// Browser Sync
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  done();
+}
 
 
-gulp.task('default', async () => {
+gulp.task('test', async () => {
   return console.log('Gulp is running...')
 }) // Üstteki gibi fonksiyon adı vermeden çalıştırmak için task ismine default yazıp konsola: gulp
 
+const compileSass = () => gulp.watch(paths.styles.src, gulp.series(sass, reload));
+const compileViews = () => gulp.watch(paths.views.src, gulp.series(fileinclude, reload));
+
+const dev = gulp.series(compileSass, compileViews, serve);
+
+export default dev;
 
 // GULP WATCH
 gulp.task('watch', async () => {
